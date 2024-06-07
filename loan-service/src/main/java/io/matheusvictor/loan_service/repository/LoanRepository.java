a576e2fae6eb3520cc0b28f41e2d51ccb02b1376
MatheusVict <matheusvictorhenrique@gmail.com>
2024-06-06 22:13:44 -0300

    feat: add all stack tracer to services, besides, loki, tempo and grafana config
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- loan-service/src/main/java/io/matheusvictor/loan_service/repository/LoanRepository.java
+++ loan-service/src/main/java/io/matheusvictor/loan_service/repository/LoanRepository.java
@@ -1,7 +1,11 @@
 package io.matheusvictor.loan_service.repository;
 
 import io.matheusvictor.loan_service.entity.Loan;
+import io.micrometer.observation.annotation.Observed;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+@Observed
 public interface LoanRepository extends JpaRepository<Loan, Long> {
 }
